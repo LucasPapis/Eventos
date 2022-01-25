72caad6c2cd6632285bc2ce830bc1f17043e3fa2
LucasPapis <lucas.papis@gmail.com>
2022-01-25 08:12:32 -0300

    Criação do banco de dados com migrations e mundança para a api operar com o Sqlite

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,39 +13,21 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[]{
-            new Evento(){
-                    EventoId = 1,
-                    Tema = "Cursão Angular",
-                    Local = "São Paulo",
-                    Lote = "1° Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                    ImagemURL = "foto.png"
-              },
-              new Evento(){
-                    EventoId = 2,
-                    Tema = "Cursão brabo",
-                    Local = "São Paulo",
-                    Lote = "2° Lote",
-                    QtdPessoas = 600,
-                    DataEvento = DateTime.Now.AddDays(5).ToString("dd/MM/yyyy"),
-                    ImagemURL = "benis.png"
-              }
-        };
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
         [HttpPost]
         public string Post()
@@ -59,8 +42,8 @@ namespace ProEventos.API.Controllers
         [HttpDelete("{id}")]
         public string delete(int id)
         {
-             return $"Delete com id = {id}";
+            return $"Delete com id = {id}";
         }
-        
+
     }
 }
